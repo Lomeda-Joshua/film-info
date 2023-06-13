875c1804d7af8240ca802968a3bcb22bec1f4c66
lomeda-joshua <joshualomeda.e14@gmail.com>
2023-06-14 05:41:19 +0800

    Removed core parts from third party packages for testing on published site

--- src/App.js
+++ src/App.js
@@ -1,28 +1,25 @@
-/** React providers **/  
-import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from 'react-router-dom';
+// /** React providers **/  
+// import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from 'react-router-dom';
 
-/** React Query **/  
-import { QueryClientProvider,QueryClient } from '@tanstack/react-query'; 
+// /** React Query **/  
+// import { QueryClientProvider,QueryClient } from '@tanstack/react-query'; 
 
-/** Root layout **/  
-import RootLayout from './pages/root-layouts/RootLayout';
+// /** Root layout **/  
+// import RootLayout from './pages/root-layouts/RootLayout';
 
-/** Pages routing **/  
-import HomePage from './pages/HomePage';
-import TvShows from './pages/TvShows';
-import SavedPage from './pages/SavedPage';
+// /** Pages routing **/  
+// import HomePage from './pages/HomePage';
+// import TvShows from './pages/TvShows';
+// import SavedPage from './pages/SavedPage';
 
-
-/** Error handling **/ 
-import ErrorPage from './pages/error-pages/ErrorPage'; 
-import NotFound from './pages/error-pages/NotFound';
+// /** Error handling **/ 
+// import ErrorPage from './pages/error-pages/ErrorPage'; 
+// import NotFound from './pages/error-pages/NotFound';
 
 import './App.css';
 
 
-const queryClient = new QueryClient();
-
-const router = createBrowserRouter(
+/* const router = createBrowserRouter(
   createRoutesFromElements(
     <Route element={<RootLayout/>} errorElement={<ErrorPage />} >      
         <Route path="/film-info" element={<HomePage />} />
@@ -31,13 +28,16 @@ const router = createBrowserRouter(
         <Route path="*" element={<NotFound />} />
     </Route>
   )
-)
+) */
 
 function App() {
+
+
   return (
     <div className = "App">
         {/* <QueryClientProvider client={queryClient}> */}
-          <RouterProvider router = {router} />      
+          {/* <RouterProvider router = {router} />       */}
+          <h1>Hello world</h1>
         {/* </QueryClientProvider> */}
     </div>
   );
