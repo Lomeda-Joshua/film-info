fb2099c3a633fbcb7188f0e9c71d48571b2390b3
lomeda-joshua <joshualomeda.e14@gmail.com>
2023-06-14 10:33:22 +0800

    Found a fix for the template bug pushing for test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 /** React providers **/  
 import {createBrowserRouter, createRoutesFromElements, RouterProvider, Route} from 'react-router-dom';
 
-/** React Query **/  
-import { QueryClientProvider,QueryClient } from 'react-query'; 
-import { ReactQueryDevtools } from 'react-query/devtools';
-
 /** Root layout **/  
 import RootLayout from './pages/root-layouts/RootLayout';
 
@@ -17,11 +13,23 @@ import ErrorPage from './pages/error-pages/ErrorPage';
 /** stylesheet **/  
 import './App.css';
 
-function App() {
+
+function App(){
+
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route element={<RootLayout/>} errorElement={<ErrorPage />} >
+          <Route path="/film-info" element={<HomePage />} />
+          
+      </Route>
+    )
+  )
+
 
   return (
     <div className="App">
-       <h1 style={{color:"white"}}>Hello world</h1>  
+       <RouterProvider router = {router} />  
     </div>
   );
 }
